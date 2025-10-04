4117c28a560d05050013213fa15047119ade73ff
Aayush Saajal <ayushsaajal@gmail.com>
2025-10-04 18:06:50 +0530

    feat: Login API

--- re_attendance_backend/models/User.js
+++ re_attendance_backend/models/User.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    code: { type: String, required: true, unique: true },
+    role: { 
+        type: String, 
+        required: true,
+        enum: ['worker', 'supervisor', 'management', 'admin'] 
+    },
+    supervisor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null }
 });
 
 module.exports = mongoose.model("User", UserSchema);
